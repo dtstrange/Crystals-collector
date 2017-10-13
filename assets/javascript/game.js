872cd0f41fd51f8bff85ecab4d37000e4d12866f
dtstrange <dustintstrange@gmail.com>
2017-10-13 13:10:46 -0700

    java main game is working reset button works

---
+++ assets/javascript/game.js
@@ -0,0 +1,63 @@
+var game = {
+    //global variables
+    winCounter: 0,
+    loseCounter: 0,
+    userAmount: 0,
+    gameAmount: 0,
+    crystalRandomValue: function() {
+        $("#red").attr("data-num", Math.floor(Math.random() * 12) + 1)
+        $("#blue").attr("data-num", Math.floor(Math.random() * 12) + 1)
+        $("#yellow").attr("data-num", Math.floor(Math.random() * 12) + 1)
+        $("#green").attr("data-num", Math.floor(Math.random() * 12) + 1)
+        game.gameAmount = Math.floor(Math.random() * 120) + 19
+        $("#points").text(game.gameAmount)
+    },
+    addScore: function(x) {
+        game.userAmount += x
+        if (game.userAmount > game.gameAmount) {
+            game.loseCounter++
+            game.gameReset()
+        } else if (game.userAmount === game.gameAmount) {
+            game.winCounter++
+            game.gameReset()
+        }
+        $("#counters").empty()
+        $("#counters").append("<p>Wins: " + game.winCounter + "</p>")
+        $("#counters").append("<p>Losses: " + game.loseCounter + "</p>")
+        $("#score").text(game.userAmount)
+
+
+    },//end addScore fucntion
+    gameReset: function(){
+    	$("#score").empty()
+    	game.crystalRandomValue()
+    }//end gameReset function
+
+} //end game object
+
+
+$(document).ready(function() {
+    // body...
+    game.crystalRandomValue()
+    //onlick functions
+    $("#red").on("click", function() {
+        var redNum = $("#red").data("num")
+        game.addScore(redNum)
+    });
+
+    $("#blue").on("click", function() {
+        var blueNum = $("#blue").data("num")
+        game.addScore(blueNum)
+    });
+
+    $("#yellow").on("click", function() {
+        var yellowNum = $("#yellow").data("num")
+        game.addScore(yellowNum)
+    });
+
+    $("#green").on("click", function() {
+        var greenNum = $("#green").data("num")
+        game.addScore(greenNum)
+    });
+    $("#resetbtn").on("click", game.gameReset);
+});
